8b7592ef647b15e93042a98cb9b5bd45e2456205
Eric <guiimeris@hotmail.com>
2025-02-18 10:29:12 -0300

    Primeiro commit: adicionei o arquivo app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+// Este é um comentário. Ele não afeta o código, mas ajuda a explicar o que está acontecendo.
+
+// Vamos criar uma variável chamada "mensagem" e armazenar um texto nela.
+let mensagem = "Olá, mundo! Esta é minha primeira aplicação Javascript";
+
+//Agora, vamos exibir a mensagem no console.
+console.log(mensagem);
